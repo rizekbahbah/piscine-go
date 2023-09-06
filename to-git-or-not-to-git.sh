bbd97bf64b0238e17137bfd552e5e6245defcde8
adam-08-02 <adam-08-02@adam-08-02s-Mac-mini.local>
2023-09-06 14:22:11 +0300

    task3

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,11 +1,3 @@
 #! /bin/bash
 
-json_file="superhero.json"
-superhero_id=170
-result-$(jq -r ".[] | select(.id == $superhero_id) | .name, ,power, .gender" "$json_file")
-if [[ -n "$result" ]];then
-    printf "$result" | tr '\n''' 
-    printf
-else
-    printf "Superhero with ID $superhero_id not found." >&2
-    if
+curl https://adam-jerusalem.nd.edu/assets/superhero/all.json | jq '.[] | select (.id == 170)| /name
