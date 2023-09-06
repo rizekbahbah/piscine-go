d3292870c94c7524eae8864e037cbcfb8e5ba120
adam-08-02 <adam-08-02@adam-08-02s-Mac-mini.local>
2023-09-06 14:23:31 +0300

    task3

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
-curl https://adam-jerusalem.nd.edu/assets/superhero/all.json | jq '.[] | select (.id == 170)| /name
+curl https://adam-jerusalem.nd.edu/assets/superhero/all.json | jq '.[] | select ( .id == 170) | .name '
+
+curl https://adam-jerusalem.nd.edu/assets/superhero/all.json | jq '.[] | select ( .id == 170) | .powerstats.power '
+
+curl https://adam-jerusalem.nd.edu/assets/superhero/all.json | jq '.[] | select ( .id == 170) | .appearance.gender '
