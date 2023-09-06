67ef9a39610d56260a37f2b725b5a243faee9082
adam-08-02 <adam-08-02@adam-08-02s-Mac-mini.local>
2023-09-06 14:43:24 +0300

    task4

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-
-curl https://adam-jerusalem.nd.edu/assets/superhero/all.json | jq " .[] | select( .id == 70) | .name'
+name=$(curl -s https://adam-jerusalem.nd.edu/assets/superhero/all.json | jq " .[] | select(.id == 70) | .name")
+echo  "$name"
