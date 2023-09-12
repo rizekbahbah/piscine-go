eda89743b78dc9c301d80b75a436a43247ad341b
adam-08-02 <adam-08-02@adam-08-02s-Mac-mini.local>
2023-09-12 16:15:52 +0300

    safaf

---
+++ isupper.go
@@ -0,0 +1,16 @@
+package piscine
+
+func IsUpper(str string) bool {
+	_arr := []rune(str)
+	ln := 0
+	for i := range my_arr {
+		ln = i
+	}
+
+	for i := 0; i <= ln; i++ {
+		if _arr[i] < 'A' || _arr[i] > 'Z' {
+			return false
+		}
+	}
+	return true
+}
