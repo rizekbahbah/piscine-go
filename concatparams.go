ff72aa0b26ef6f3f343fc6cbec665d20f6c0e38d
adam-08-02 <adam-08-02@adam-08-02s-Mac-mini.local>
2023-09-14 17:06:35 +0300

    safaf

--- concatparams.go
+++ concatparams.go
@@ -1,17 +1,17 @@
 package piscine
 
 func ConcatParams(args []string) string {
-	Sts := ""
+	mySts := ""
 	ln := 0
 	for i := range args {
 		ln = i
 	}
 	for i := 0; i <= ln; i++ {
 		if i != ln {
-			Str = Str + args[i] + "\n"
+			myStr = myStr + args[i] + "\n"
 		} else {
-			Str = Str + args[i]
+			myStr = myStr + args[i]
 		}
 	}
-	return Str
+	return myStr
 }
