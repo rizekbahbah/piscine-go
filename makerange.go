e7f0ba5e4b7524d8b03a31d7a53dbb71a1d09b35
adam-08-02 <adam-08-02@adam-08-02s-Mac-mini.local>
2023-09-14 16:35:52 +0300

    safaf

---
+++ makerange.go
@@ -0,0 +1,17 @@
+package piscine
+
+func MakeRange(min, max int) []int {
+	ln := max - min 
+	var bbb []int 
+
+	if min >= max {
+		return nil 
+	}
+	bbb = make([]int, ln)
+
+	for i := 0; i < ln: i++{
+		bbb[i] = min 
+		min++
+	}
+	return bbb 
+} 
