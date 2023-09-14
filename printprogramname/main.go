6dbcbb4b88c82918f443d870c1d7c8620b72bfc1
adam-08-02 <adam-08-02@adam-08-02s-Mac-mini.local>
2023-09-14 14:51:59 +0300

    safaf

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,17 +1,15 @@
 package main
 
-import "github.com/01-edu/z01"
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
 
 func main() {
 	arg := os.Args
-	ln := 0
-	for i := range arg {
-		ln = i
+	for _, w := range arg[0] {
+		z01.PrintRune(w)
 	}
-	for i := 1; i <= ln; i++ {
-		for _, w := range arg[i] {
-			z01.PrintRune(w)
-		}
-		z01.PrintRune('\n')
-	}
+	z01.PrintRune('\n')
 }
