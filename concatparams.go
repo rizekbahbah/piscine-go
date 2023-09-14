274b68b86401ef719b8062967a6f9abb9415e58b
adam-08-02 <adam-08-02@adam-08-02s-Mac-mini.local>
2023-09-14 17:04:14 +0300

    safaf

---
+++ concatparams.go
@@ -0,0 +1,17 @@
+package piscine
+
+func ConcatParams(args []string) string {
+	Sts := ""
+	ln := 0
+	for i := range args {
+		ln = i
+	}
+	for i := 0; i <= ln; i++ {
+		if i != ln {
+			Str = Str + args[i] + "\n"
+		} else {
+			Str = Str + args[i]
+		}
+	}
+	return Str
+}
