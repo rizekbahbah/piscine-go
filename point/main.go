6508973b3039118c94e02bc816575999c6156791
adam-08-02 <adam-08-02@adam-08-02s-Mac-mini.local>
2023-09-18 16:20:33 +0300

    safaf

--- point/main.go
+++ point/main.go
@@ -1,27 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"github.com/01-edu/z01"
 )
 
 type point struct {
 	x int
 	y int
-}
-
-func NumFromBase(num int, base string) string {
-	mod := len(base)
-	i := 0
-	if num == 0 {
-		return "0"
-	}
-	for j := 1; j <= num%mod; j++ {
-		i++
-	}
-	for j := -1; j >= num%mod; j-- {
-		i++
-	}
-	return NumFromBase(num/mod, base) + string(base[i])
 }
 
 func setPoint(ptr *point) {
@@ -34,9 +20,68 @@ func main() {
 
 	setPoint(points)
 
-	result := "x = " + NumFromBase(points.x, "0123456789")[1:] + ", y = " + NumFromBase(points.y, "0123456789")[1:] + "\n"
-
-	for _, c := range result {
-		z01.PrintRune(c)
-	}
+	// z01.PrintRune('x')
+	// z01.PrintRune(' ')
+	// z01.PrintRune('=')
+	// z01.PrintRune(' ')
+	// IntoRune(points.x)
+	// z01.PrintRune(',')
+	// z01.PrintRune(' ')
+	// z01.PrintRune('y')
+	// z01.PrintRune(' ')
+	// z01.PrintRune('=')
+	// z01.PrintRune(' ')
+	// IntoRune(points.y)
+	// z01.PrintRune('\n')
+	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
 }
+
+func check(r int) {
+	c := '0'
+	if r == 0 {
+		z01.PrintRune(c)
+		return
+	}
+	for i := 1; i <= r%10; i++ {
+		c++
+	}
+	for i := -1; i >= r%10; i-- {
+		c++
+	}
+	if r/10 != 0 {
+		check(r / 10)
+	}
+	z01.PrintRune(c)
+	return
+}
+
+func IntoRune(n int) {
+	if n < 0 {
+		z01.PrintRune('-')
+	}
+	check(n)
+}
+
+/*
+type student struct {
+	name string
+	age int
+}
+
+func changeName (pointer *student, nameChosen string) {
+
+	pointer.name = nameChosen
+}
+
+
+
+	chris := student{}
+
+	chris.name = "chris"
+	chris.age = 30
+
+	changeName(&chris, "lee")
+
+	ptr := point{}
+
+*/
