c7802ec240db28fa63d0ea1ad1caf081e1d920e8
adam-08-02 <adam-08-02@adam-08-02s-Mac-mini.local>
2023-09-18 16:34:27 +0300

    safaf

--- q8/boolean/main.go
+++ q8/boolean/main.go
@@ -1,52 +1,52 @@
 package main
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
-type point struct {
+type cord struct {
 	x int
 	y int
 }
 
-func setPoint(ptr *point) {
+func setPoint(ptr *cord) {
 	ptr.x = 42
 	ptr.y = 21
 }
 
+func printS(s string) {
+	for _, sym := range s {
+		z01.PrintRune(sym)
+	}
+}
+
+func intToRune(num int) {
+	if num < 10 {
+		for i, j := 0, '0'; i <= num%10; i, j = i+1, j+1 {
+			if i == num%10 {
+				z01.PrintRune(j)
+			}
+		}
+	} else {
+		nextnum := num / 10
+		mod := num % 10
+		intToRune(nextnum)
+		intToRune(mod)
+	}
+}
+
 func main() {
-	xStr := "x = "
-	yStr := "y = "
-	points := &point{}
+	pt := cord{}
+	points := pt
 
-	setPoint(points)
+	setPoint(&points)
+	printS("x = ")
+	//	printI(points.x)
+	intToRune(points.x)
 
-	xmassiv := []rune{}
-	ymassiv := []rune{}
+	printS(", y = ")
+	//	printI(points.y)
+	intToRune(points.y)
 
-	xVal := points.x
-	yVal := points.y
-
-	for xVal != 0 {
-		xmassiv = append(xmassiv, rune(xVal%10))
-		xVal /= 10
-	}
-	for _, val := range xStr {
-		z01.PrintRune(rune(val))
-	}
-	for i := len(xmassiv) - 1; i >= 0; i-- {
-		z01.PrintRune(xmassiv[i] + 48)
-	}
-	z01.PrintRune(',')
-	z01.PrintRune(' ')
-
-	for yVal != 0 {
-		ymassiv = append(ymassiv, rune(yVal%10))
-		yVal /= 10
-	}
-	for _, val := range yStr {
-		z01.PrintRune(rune(val))
-	}
-	for i := len(ymassiv) - 1; i >= 0; i-- {
-		z01.PrintRune(ymassiv[i] + 48)
-	}
 	z01.PrintRune('\n')
 }
