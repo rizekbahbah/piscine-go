ca048f8ced2e29f4b979dfe61fc6cb4395067ec9
adam-08-02 <adam-08-02@adam-08-02s-Mac-mini.local>
2023-09-21 13:17:01 +0300

    safaf

--- descendcomb.go
+++ descendcomb.go
@@ -3,17 +3,17 @@ package piscine
 import "github.com/01-edu/z01"
 
 func DescendComb() {
-	for i := '9'; i <= '0'; i-- {
-		for j := '9'; j <= '0'; j-- {
-			for h := '9'; h <= '0'; h-- {
-				for k := '9'; k <= '0'; k-- {
-					if i < h || (i == h && j > k) {
-						z01.PrintRune(i)
-						z01.PrintRune(j)
+	for x := '9'; x >= '0'; x-- {
+		for y := '9'; y >= '0'; y-- {
+			for z := '9'; z >= '0'; z-- {
+				for k := '9'; k >= '0'; k-- {
+					if x < z || (x == z && y > k) {
+						z01.PrintRune(x)
+						z01.PrintRune(y)
 						z01.PrintRune(32)
-						z01.PrintRune(h)
+						z01.PrintRune(z)
 						z01.PrintRune(k)
-						if !(i == '0' && j == '1' && h == '0' && k == '0') {
+						if !(x == '0' && y == '1' && z == '0' && k == '0') {
 							z01.PrintRune(44)
 							z01.PrintRune(32)
 						}
