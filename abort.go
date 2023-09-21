7af634948559292bc1af09fa86794a286051c816
adam-08-02 <adam-08-02@adam-08-02s-Mac-mini.local>
2023-09-21 16:54:56 +0300

    safaf

--- abort.go
+++ abort.go
@@ -1,9 +1,17 @@
 package piscine
 
-import "sort"
-
 func Abort(a, b, c, d, e int) int {
-	tab := []int{a, b, c, d, e}
-	sort.Ints(tab)
-	return tab[2]
+	array := []int{a, b, c, d, e}
+	count := 5
+	temp := 0
+	for i := 0; i < count-1; i++ {
+		temp = i
+		for j := i + 1; j < count; j++ {
+			if array[j] < array[temp] {
+				temp = j
+			}
+		}
+		array[i], array[temp] = array[temp], array[i]
+	}
+	return array[2]
 }
