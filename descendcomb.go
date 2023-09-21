9c18d675f958babf273b850ee261c6b61cc23ce0
adam-08-02 <adam-08-02@adam-08-02s-Mac-mini.local>
2023-09-21 13:19:20 +0300

    safaf

--- descendcomb.go
+++ descendcomb.go
@@ -7,15 +7,15 @@ func DescendComb() {
 		for y := '9'; y >= '0'; y-- {
 			for z := '9'; z >= '0'; z-- {
 				for k := '9'; k >= '0'; k-- {
-					if x < z || (x == z && y > k) {
+					if (x < z) || (x == z && y > k) {
 						z01.PrintRune(x)
 						z01.PrintRune(y)
-						z01.PrintRune(32)
+						z01.PrintRune(' ')
 						z01.PrintRune(z)
 						z01.PrintRune(k)
 						if !(x == '0' && y == '1' && z == '0' && k == '0') {
-							z01.PrintRune(44)
-							z01.PrintRune(32)
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
 						}
 
 					}
