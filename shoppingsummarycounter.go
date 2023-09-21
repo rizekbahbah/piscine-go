23b0af607b380f42983d4b746b78e164bba9c997
adam-08-02 <adam-08-02@adam-08-02s-Mac-mini.local>
2023-09-21 18:38:20 +0300

    safaf

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -1,21 +1,49 @@
 package piscine
 
-func ShoppingSummaryCounter(str string) map[string]int {
-	// Split the string into a slice of items.
-	// Create a map to store the item counts.
-	summary := make(map[string]int)
+// import "fmt"
 
-	// Iterate over the items and count their occurrences.
-	for _, item := range items {
-		if item[0] == '-' {
-			// Get the item name.
-			itemName := item[1:]
+func splitItems(s string) []string {
+	var items []string
+	var currentItem string
 
-			// Increment the count for the item.
-			summary[itemName]++
+	if s == " " {
+		items = append(items, "")
+		items = append(items, "")
+		return items
+	}
+	if s == "" {
+		items = append(items, "")
+		return items
+	}
+	for i, ch := range s {
+		if ch == ' ' && i == len(s)-1 {
+			items = append(items, "")
+		}
+		if ch != ' ' {
+			currentItem += string(ch)
+		} else if currentItem != "" {
+			items = append(items, currentItem)
+			currentItem = ""
+		} else {
+			// This condition handles consecutive spaces and treats them as a separate item.
+			items = append(items, "")
 		}
 	}
 
-	// Return the summary map.
+	if currentItem != "" {
+		items = append(items, currentItem)
+	}
+
+	return items
+}
+
+func ShoppingSummaryCounter(str string) map[string]int {
+	items := splitItems(str)
+	summary := make(map[string]int)
+
+	for _, item := range items {
+		summary[item]++
+	}
+
 	return summary
 }
