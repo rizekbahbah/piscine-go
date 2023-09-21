d34aa80808cc6da3c6e1a46ebb73317b90e72aca
adam-08-02 <adam-08-02@adam-08-02s-Mac-mini.local>
2023-09-21 17:56:58 +0300

    safaf

---
+++ shoppingsummarycounter.go
@@ -0,0 +1,22 @@
+package piscine
+
+func ShoppingSummaryCounter(str string) map[string]int {
+	items := strings.Split(str, "\n")
+
+	// Create a map to store the item counts.
+	summary := make(map[string]int)
+
+	// Iterate over the items and count their occurrences.
+	for _, item := range items {
+		if item[0] == '-' {
+			// Get the item name.
+			itemName := item[1:]
+
+			// Increment the count for the item.
+			summary[itemName]++
+		}
+	}
+
+	// Return the summary map.
+	return summary
+}
