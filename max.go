91244830ea773f59784d0297de407fc91870bb92
adam-08-02 <adam-08-02@adam-08-02s-Mac-mini.local>
2023-09-22 14:05:30 +0300

    sjdkks

--- max.go
+++ max.go
@@ -1,8 +1,11 @@
 package piscine
 
-import "sort"
-
 func Max(arr []int) int {
-	sort.Ints(arr)
-	return arr[len(arr)-1]
+	max := arr[0]
+	for i := 1; i < len(arr); i++ {
+		if max < arr[i] {
+			max = arr[i]
+		}
+	}
+	return max
 }
