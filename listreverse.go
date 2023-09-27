385b133680694c65fc35def25c70bab7824f57c9
Rizek Bahbah <rbabuanis@hotmail.com>
2023-09-27 17:40:34 +0300

    x

---
+++ listreverse.go
@@ -0,0 +1,26 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListReverse(l *List) {
+	current := l.Head
+	prev := l.Head
+	prev = nil
+	for current != nil {
+		next := current.Next
+		current.Next = prev
+		prev = current
+		current = next
+	}
+	temp := l.Head
+	l.Head = l.Tail
+	l.Tail = temp
+}
