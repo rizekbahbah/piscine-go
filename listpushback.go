bc944bb7392b13bdead9b67843cd2fa181da5e03
Rizek Bahbah <rbabuanis@hotmail.com>
2023-09-27 17:05:59 +0300

    x

---
+++ listpushback.go
@@ -0,0 +1,15 @@
+package piscine
+
+func ListPushBack(l *List, data interface{}) {
+	n := &NodeL{Data: data}
+	if l.Head == nil {
+		l.Head = n
+	} else {
+		listItem := l.Head
+		for listItem.Next != nil {
+			listItem = listItem.Next
+		}
+		listItem.Next = n
+		l.Tail = n
+	}
+}
