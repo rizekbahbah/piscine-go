9b89490244c59f346251095f6ea8f1f1374a30f6
Rizek Bahbah <rbabuanis@hotmail.com>
2023-09-27 17:15:33 +0300

    x

--- listpushfront.go
+++ listpushfront.go
@@ -1,12 +1,40 @@
 package piscine
 
-func ListPushFront(l *List, data interface{}) {
-	node := &NodeL{Data: data}
-
-	if l.Head == nil {
-		l.Head = node
-	} else {
-		node.Next = l.Head
-		l.Head = node
-	}
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
 }
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushFront(l *List, data interface{}) {
+	val := &NodeL{Data: data}
+	if l.Head == nil {
+		l.Head = val
+		return
+	}
+	val.Next = l.Head
+	l.Head = val
+}
+
+// func main() {
+
+// 	link := &List{}
+
+// 	ListPushFront(link, "Hello")
+// 	ListPushFront(link, "man")
+// 	ListPushFront(link, "how are you")
+// 	ListPushFront(link, "1")
+// 	ListPushFront(link, "2")
+// 	ListPushFront(link, "3")
+
+// 	it := link.Head
+// 	for it != nil {
+// 		fmt.Print(it.Data, " ")
+// 		it = it.Next
+// 	}
+// 	fmt.Println()
+// }
